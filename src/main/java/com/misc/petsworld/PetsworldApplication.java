1a53019825b5d75be5811cadbba1e31ab5b20cf7
Serafeim Oikonomou <serafeimo@gmail.com>
2024-10-05 17:33:36 +0300

    Setup project

---
+++ src/main/java/com/misc/petsworld/PetsworldApplication.java
@@ -0,0 +1,13 @@
+package com.misc.petsworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misc/petsworld/PetsworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc.petsworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
